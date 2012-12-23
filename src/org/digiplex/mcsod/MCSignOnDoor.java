0496434a61578f4f6838c58f635d1b8bf0926e8b
Sam Archer <greeneggsandsamuel@gmail.com>
2012-12-23 01:55:16 -0800

    Updated for 1.4.6

--- src/org/digiplex/mcsod/MCSignOnDoor.java
+++ src/org/digiplex/mcsod/MCSignOnDoor.java
@@ -55,7 +55,7 @@ public class MCSignOnDoor {
 	
 	static { //static constructor
 		String protoversion = MCSignOnDoor.class.getPackage().getSpecificationVersion();
-		if (protoversion == null) protoversion = /****/ "49" /****/; //up to date protocol version - UPDATE MANIFEST TOO!
+		if (protoversion == null) protoversion = /****/ "51" /****/; //up to date protocol version - UPDATE MANIFEST TOO!
 		CURRENT_PROTOCOL_VERSION = Integer.parseInt(protoversion);
 	}
 	
@@ -789,6 +789,7 @@ public class MCSignOnDoor {
 					case 39: //CASE 39: this is for version 1.3.1, introduction of the protocol version
 					case 47: //CASE 47: this is for version 1.4.2, identical for this part here
 					case 49: //CASE 49: version 1.4.4, no change to protocol
+					case 51: //CASE 51: version 1.4.6, no change that I'm aware of
 					{
 						in.read(inbyte, 2, 2); //read 16-byte number, message length
 						int len = parseChar(inbyte, 2);
